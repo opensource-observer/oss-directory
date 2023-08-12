8038f6f89ac3928a0103d6fbdd8ef7a1a8560667
Raymond Cheng <me@raymondcheng.net>
2023-08-11 22:30:10 -0700

    fix: just some minor cleanup
    * Fix compiler bugs, minor reorganization

--- src/scripts/optimism-attestation.ts
+++ src/scripts/optimism-attestation.ts
@@ -1,27 +1,31 @@
-import { EAS, SchemaEncoder } from '@ethereum-attestation-service/eas-sdk';
-import { ethers } from 'ethers';
-import { promises as fs } from 'fs';
-import YAML from 'yaml';
+import * as dotenv from "dotenv";
+import { EAS, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
+import { ethers } from "ethers";
+import { ensure } from "../utils/common.js";
+import { readProjectFile } from "../validator/index.js";
+import { BlockchainAddress } from "../types/blockchain-address.js";
+dotenv.config();
 
 const addresses: any = {
-  'optimism-goerli': {
-    EASContractAddress: '0x1a5650d0ecbca349dd84bafa85790e3e6955eb84',
-    schemaUID: '0x739257b1bf8533a29a5c59a6dda5905c50f7c2bf436d709cd9ea7bfabbe5172b'
+  "optimism-goerli": {
+    EASContractAddress: "0x1a5650d0ecbca349dd84bafa85790e3e6955eb84",
+    schemaUID:
+      "0x739257b1bf8533a29a5c59a6dda5905c50f7c2bf436d709cd9ea7bfabbe5172b",
   },
-  'optimism': {
-    EASContractAddress: '0x4200000000000000000000000000000000000021',
-    schemaUID: '' // TODO: update once the schema has been deployed on OP Mainnet
-  }
+  optimism: {
+    EASContractAddress: "0x4200000000000000000000000000000000000021",
+    schemaUID: "", // TODO: update once the schema has been deployed on OP Mainnet
+  },
 };
 
 const rpcUrls: Record<string, string> = {
-  'optimism-goerli': 'https://goerli.optimism.io',
-  'optimism': 'https://mainnet.optimism.io'
+  "optimism-goerli": "https://goerli.optimism.io",
+  optimism: "https://mainnet.optimism.io",
 };
 
 type AttestInput = {
-  privateKey: string;
-  network: string;
+  privateKey?: string;
+  network?: string;
   repoUrl: string;
   address: string;
   addressType: string;
@@ -31,38 +35,43 @@ export async function attest(input: AttestInput) {
   const { privateKey, network, repoUrl, address, addressType } = input;
 
   if (!privateKey) {
-    throw new Error('privateKey is required');
+    throw new Error("privateKey is required");
   }
 
   if (!network) {
-    throw new Error('network is required');
+    throw new Error("network is required");
   }
 
   const rpcUrl = rpcUrls[network];
 
   if (!repoUrl) {
-    throw new Error('repoUrl is required');
+    throw new Error("repoUrl is required");
   }
 
-  const githubRepoUrlPattern = /^(https:\/\/)?(www\.)?github\.com\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+(\/)?$/;
+  const githubRepoUrlPattern =
+    /^(https:\/\/)?(www\.)?github\.com\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+(\/)?$/;
   if (!githubRepoUrlPattern.test(repoUrl)) {
-    throw new Error('repoUrl is not a valid GitHub URL');
+    throw new Error("repoUrl is not a valid GitHub URL");
   }
 
   if (!address) {
-    throw new Error('address is required');
+    throw new Error("address is required");
   }
 
   const validAddressTypes = ["eoa", "safe", "creator", "factory", "contract"];
   if (!addressType || !validAddressTypes.includes(addressType)) {
-    throw new Error(`addressType must be one of ${validAddressTypes.join(", ")}`);
+    throw new Error(
+      `addressType must be one of ${validAddressTypes.join(", ")}`,
+    );
   }
 
   const provider = new ethers.providers.StaticJsonRpcProvider(rpcUrl);
   const signer = new ethers.Wallet(privateKey, provider);
   const EASContractAddress = addresses[network].EASContractAddress;
   if (!EASContractAddress) {
-    throw new Error(`EASContractAddress is not available for network "${network}"`);
+    throw new Error(
+      `EASContractAddress is not available for network "${network}"`,
+    );
   }
   const eas = new EAS(EASContractAddress);
   eas.connect(signer);
@@ -72,17 +81,19 @@ export async function attest(input: AttestInput) {
     throw new Error(`schemaUID is not available for network "${network}"`);
   }
 
-  const schemaEncoder = new SchemaEncoder('string repositoryUrl, address address, bytes32 addressType');
+  const schemaEncoder = new SchemaEncoder(
+    "string repositoryUrl, address address, bytes32 addressType",
+  );
   const encodedData = schemaEncoder.encodeData([
-    { name: 'repositoryUrl', value: repoUrl, type: 'string' },
-    { name: 'address', value: address, type: 'address' },
-    { name: 'addressType', value: addressType, type: 'bytes32' },
+    { name: "repositoryUrl", value: repoUrl, type: "string" },
+    { name: "address", value: address, type: "address" },
+    { name: "addressType", value: addressType, type: "bytes32" },
   ]);
 
   const res = await eas.attest({
     schema: schemaUID,
     data: {
-      recipient: '0x0000000000000000000000000000000000000000',
+      recipient: "0x0000000000000000000000000000000000000000",
       expirationTime: 0,
       revocable: true,
       data: encodedData,
@@ -94,78 +105,58 @@ export async function attest(input: AttestInput) {
 
   return {
     hash,
-    uid: newAttestationUID
+    uid: newAttestationUID,
   };
 }
 
-type YAMLData = {
-  version: number;
-  slug: string;
-  name: string;
-  github: Array<{ url: string }>;
-  optimism: Array<{ address: string, type: string }>;
-};
+async function getData(
+  filePath: string,
+): Promise<{ network: string; attestations: AttestInput[] }> {
+  const project = await readProjectFile(filePath);
 
-async function getYAMLData(filePath: string): Promise<{ network: string, attestations: AttestInput[] }> {
-  const yamlContent = await fs.readFile(filePath, 'utf-8');
+  const githubList = ensure(
+    project.github,
+    "No GitHub URLs found in the project file",
+  );
+  const githubRepoUrl = githubList[0].url; // Assumes the first URL is the monorepo or owner URL
 
-  let data: YAMLData;
-  try {
-    data = YAML.parse(yamlContent) as YAMLData;
-  } catch (error) {
-    throw new Error("Failed to parse YAML data. Ensure it has the correct structure.");
-  }
-
-  const githubRepoUrl = data.github[0].url;  // Assumes the first URL is the monorepo or owner URL
-
-  const attestations = data.optimism.map((item: any) => {
+  const optimismList = ensure(
+    project.optimism,
+    "No Optimism addresses found in the project file",
+  );
+  const attestations = optimismList.map((item: BlockchainAddress) => {
     if (!ethers.utils.isAddress(item.address)) {
       throw new Error(`Invalid Ethereum address: ${item.address}`);
     }
     return {
       repoUrl: githubRepoUrl,
       address: item.address,
-      addressType: item.type
+      addressType: item.type,
     };
   });
 
   return {
-    network: 'optimism',  // Currently hardcoded
-    attestations
+    network: "optimism", // Currently hardcoded
+    attestations,
   };
 }
 
-async function main(filePath: string) {
-  const inputData = await getYAMLData(filePath);
+async function main() {
+  const args = process.argv.slice(2);
+  if (args.length !== 1) {
+    console.error("Please provide a path to the YAML input file.");
+    process.exit(1);
+  }
+  const filePath = args[0];
+
+  const inputData = await getData(filePath);
   for (const attestation of inputData.attestations) {
     const result = await attest({
       ...attestation,
       privateKey: process.env.PRIVATE_KEY,
-      network: inputData.network
+      network: inputData.network,
     });
-    console.log('result:', result);
+    console.log("result:", result);
   }
 }
-
-if (require.main === module) {
-  require('dotenv').config();
-
-  const args = process.argv.slice(2);
-  if (args.length !== 1) {
-    console.error('Please provide a path to the YAML input file.');
-    process.exit(1);
-  }
-
-  const filePath = args[0];
-
-  (async () => {
-    try {
-      await fs.stat(filePath);
-    } catch (err) {
-      console.error('File not found:', filePath);
-      process.exit(1);
-    }
-
-    await main(filePath);
-  })().catch(console.error);
-}
+main();
