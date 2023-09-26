e5893419e8069a6c0a22dda770bea107a676f5e5
ccerv1 <carl.cervone@gmail.com>
2023-09-26 11:32:57 -0400

    fix: schema updates and pipe handling

--- src/scripts/summarize.ts
+++ src/scripts/summarize.ts
@@ -2,35 +2,53 @@ import fs from "fs";
 import path from "path";
 import { parse } from "yaml";
 
+interface ProjectData {
+  version: number;
+  slug: string;
+  name: string;
+  github: { url: string }[];
+  npm: { url: string }[];
+  blockchain: {
+    address: string;
+    name?: string;
+    tags: string[];
+    networks: string[];
+  }[];
+}
+
 const walk = (
   dir: string,
   done: (err: Error | null, results?: string[]) => void,
   filter?: (f: string) => boolean,
 ): void => {
   let results: string[] = [];
-  fs.readdir(dir, { withFileTypes: true }, (err: Error | null, list: any[]) => {
-    if (err) return done(err);
+  fs.readdir(
+    dir,
+    { withFileTypes: true },
+    (err: Error | null, list: fs.Dirent[]) => {
+      if (err) return done(err);
 
-    let pending = list.length;
-    if (!pending) return done(null, results);
+      let pending = list.length;
+      if (!pending) return done(null, results);
 
-    list.forEach((file) => {
-      const filePath = path.resolve(dir, file.name);
-      if (file.isDirectory()) {
-        walk(
-          filePath,
-          (err, res) => {
-            if (res) results = results.concat(res);
-            if (!--pending) done(null, results);
-          },
-          filter,
-        );
-      } else {
-        if (!filter || filter(filePath)) results.push(filePath);
-        if (!--pending) done(null, results);
-      }
-    });
-  });
+      list.forEach((file) => {
+        const filePath = path.resolve(dir, file.name);
+        if (file.isDirectory()) {
+          walk(
+            filePath,
+            (err, res) => {
+              if (res) results = results.concat(res);
+              if (!--pending) done(null, results);
+            },
+            filter,
+          );
+        } else {
+          if (!filter || filter(filePath)) results.push(filePath);
+          if (!--pending) done(null, results);
+        }
+      });
+    },
+  );
 };
 
 const processFiles = (err: Error | null, files?: string[]) => {
@@ -49,24 +67,23 @@ const processFiles = (err: Error | null, files?: string[]) => {
 
     for (const file of files.sort()) {
       const content = fs.readFileSync(file, "utf8");
-      const data = parse(content);
+      const data: ProjectData = parse(content);
 
-      const githubRepos = (data.github || []).length;
-      const blockchainAddresses = (data.optimism || []).length;
+      const githubRepos = data.github.length;
+      const blockchainAddresses = data.blockchain ? data.blockchain.length : 0;
 
       tally.projects++;
       tally.githubRepos += githubRepos;
       tally.blockchainAddresses += blockchainAddresses;
 
-      const projectName = path.basename(file, ".yaml");
+      const projectName = data.name.replace(/\|/g, "&#124;"); // Replace '|' with '&#124;'
       const relativePath = path.relative(".", file);
+      const githubLink = `https://github.com/hypercerts-org/oss-directory/tree/main/${relativePath.replace(
+        /\\/g,
+        "/",
+      )}`;
       markdownRows.push(
-        `| [${
-          data.name || projectName
-        }](https://github.com/hypercerts-org/oss-directory/tree/main/${relativePath.replace(
-          /\\/g,
-          "/",
-        )}) | ${githubRepos} | ${blockchainAddresses} |`,
+        `| [${projectName}](${githubLink}) | ${githubRepos} | ${blockchainAddresses} |`,
       );
     }
 
