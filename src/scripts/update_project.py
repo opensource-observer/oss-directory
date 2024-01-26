6bcd46b19b6daa70e08e1a0fbde188075f14f76d
Carl Cervone <carl.cervone@gmail.com>
2024-01-25 21:34:14 -0500

    smol refactor: updater

--- src/scripts/update_project.py
+++ src/scripts/update_project.py
@@ -11,6 +11,7 @@ LOCAL_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "data", "projec
 
 logging.basicConfig(level=logging.INFO, filename="update_project.log", filemode="w", datefmt="%Y-%m-%d %H:%M:%S", format="%(asctime)-15s %(levelname)-8s %(message)s")
 
+
 def replace_single_quotes_with_double_quotes_in_file(file_path):
     try:
         with open(file_path, 'r') as file:
@@ -21,7 +22,6 @@ def replace_single_quotes_with_double_quotes_in_file(file_path):
         with open(file_path, 'w') as file:
             file.write(modified_content)
 
-        logging.info(f'Successfully replaced single quotes with double quotes in {file_path}')
     except Exception as e:
         logging.error(f'Error replacing single quotes with double quotes in {file_path}: {e}')
 
@@ -52,9 +52,49 @@ def append_github_urls(filepath: str, github_url: str) -> None:
     yaml_url_data.append(url)
     project_data["urls"] = yaml_url_data
     dump(project_data, filepath)
-    logging.info(f"Updated {filepath}")
+    logging.info(f"Updated {filepath.replace(LOCAL_PATH, '')}")
     replace_single_quotes_with_double_quotes_in_file(filepath)
 
+
+def update_address(project_slug: str, address: str, name: str, networks: list, tags: list) -> None:
+    """
+    Update an address in a project YAML file. The function only updates existing entries.
+
+    Args:
+    project_slug (str): The project slug.
+    address (str): The address to update.
+    name (str): The name of the address.
+    networks (list): The networks of the address.
+    tags (list): The tags of the address.
+    """
+
+    project_path = os.path.join(LOCAL_PATH, project_slug[0], f"{project_slug}.yaml")
+    if not os.path.exists(project_path):
+        logging.warning(f"File {project_path} does not exist.")
+        return False
+    
+    project_data = load_yaml_data(project_path)
+    if not project_data:
+        logging.error(f"Error loading YAML data at {project_path}.")
+        return False
+
+    yaml_address_data = project_data.get("blockchain", [])
+    updated = False
+    for entry in yaml_address_data:                    
+        if entry['address'].lower() == address.lower():
+            if name and not entry.get("name"):
+                entry["name"] = name
+            entry["networks"] = list(set(entry.get("networks", []) + networks))
+            entry["tags"] = list(set(entry.get("tags", []) + tags))
+            logging.info(f"Updated {address} in {project_slug[0]}/{project_slug}")
+            updated = True
+            break
+    
+    if updated:
+        dump(project_data, project_path)
+        logging.info(f"Dumped YAML at {project_slug[0]}/{project_slug}.yaml")
+        replace_single_quotes_with_double_quotes_in_file(project_path)
+    
 
 def update_addresses_from_json(filepath: str) -> None:
     """
@@ -69,48 +109,20 @@ def update_addresses_from_json(filepath: str) -> None:
         logging.error("File does not exist.")
         return
     
-    
     project_yaml_data = get_yaml_data_from_path(LOCAL_PATH)
     yaml_data = {data['slug']: data for data in project_yaml_data}
     with open(filepath, mode='r', encoding='utf-8') as file:
         data = json.load(file)
-        for slug, addresses in data.items():
-            project_path = os.path.join(LOCAL_PATH, slug[0], f"{slug}.yaml")
-            if not os.path.exists(project_path):
-                logging.warning(f"File {project_path} does not exist.")
-                continue
-            
-            project_data = yaml_data.get(slug)
-            yaml_address_data = project_data.get("blockchain", [])
+        for label, addresses in data.items():
             for address, details in addresses.items():
+                slug = details.get("slug")
+                if slug not in yaml_data:
+                    logging.error(f"Slug {slug} not found.")
+                    continue
                 name = details.get("name")
-                networks = details.get("networks")
-                tags = details.get("tags")
-
-                updated = False
-                for entry in yaml_address_data:                    
-                    if entry['address'].lower() == address.lower():
-                        if name and not entry.get("name"):
-                            entry["name"] = name
-                        entry["networks"] = list(set(entry.get("networks", []) + networks))
-                        entry["tags"] = list(set(entry.get("tags", []) + tags))
-                        logging.info(f"Updated {address} in {project_path}")
-                        updated = True
-                        break
-                if not updated:
-                    entry = {
-                        "address": address,
-                        "networks": networks,
-                        "tags": tags
-                    }
-                    if name:
-                        entry["name"] = name
-                    yaml_address_data.append(entry)
-                    logging.info(f"Added {address} to {project_path}")
-
-            dump(project_data, project_path)
-            logging.info(f"Dumped YAML at {project_path}")
-            replace_single_quotes_with_double_quotes_in_file(project_path)
+                networks = details.get("networks", [])
+                tags = details.get("tags", [])            
+                update_address(slug, address, name, networks, tags)
                 
 
 def main() -> None:
