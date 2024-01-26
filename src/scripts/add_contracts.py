979d57d0d8f66dca56898e442ae5dfb93977e5df
Carl Cervone <carl.cervone@gmail.com>
2024-01-26 07:28:04 -0500

    fix: remove fork contracts

--- src/scripts/add_contracts.py
+++ src/scripts/add_contracts.py
@@ -13,6 +13,39 @@ LOCAL_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "data", "projec
 logging.basicConfig(level=logging.INFO, filename="add_contracts.log", filemode="w", datefmt="%Y-%m-%d %H:%M:%S", format="%(asctime)-15s %(levelname)-8s %(message)s")
 
 
+temp_forks_to_ignore = {
+  "arbitrum:0x74c764d41b77dbbb4fe771dab1939b00b146894a": [
+    "a/abracadabra.yaml"
+  ],
+  "arbitrum:0xe9426fcf504d448cc2e39783f1d1111dc0d8e4e0": [
+    "s/sperax.yaml",
+    "p/plutusdao.yaml"
+  ],
+  "arbitrum:0x8df6efec5547e31b0eb7d1291b511ff8a2bf987c": [
+    "b/balancer.yaml"
+  ],
+  "arbitrum:0x7dfdef5f355096603419239ce743bfaf1120312b": [
+    "b/balancer.yaml"
+  ],
+  "arbitrum:0xef44d6786b2b4d544b7850fe67ce6381626bf2d6": [
+    "b/balancer.yaml"
+  ],
+  "arbitrum:0x2433477a10fc5d31b9513c638f19ee85caed53fd": [
+    "b/balancer.yaml"
+  ],
+  "arbitrum:0xcf0a32bbef8f064969f21f7e02328fb577382018": [
+    "b/balancer.yaml"
+  ],
+  "arbitrum:0xf1665e19bc105be4edd3739f88315cc699cc5b65": [
+    "b/balancer.yaml"
+  ],
+  "arbitrum:0xf23b4db826dba14c0e857029dff076b1c0264843": [
+    "b/balancer.yaml"
+  ]
+}
+
+
+
 def add_contracts_from_dune_export(filepath: str, chain: str) -> None:
     """
     Add contracts from a Dune CSV dump to a project YAML file.
@@ -45,13 +78,22 @@ def add_contracts_from_dune_export(filepath: str, chain: str) -> None:
         namespace = row['namespace']
         name = row['name']
         from_address = row['from']
-        if (namespace, name, from_address) in dups:
-            continue
         contract_address = row['address']
         if from_address in address_to_slug_map:
+            if (namespace, name, from_address) in dups:
+                continue
+            if contract_address in address_to_slug_map:
+                continue
             project_slug = address_to_slug_map[from_address]
+            if f"{chain}:{contract_address}" in temp_forks_to_ignore:
+                for fork in temp_forks_to_ignore[f"{chain}:{contract_address}"]:
+                    if project_slug in fork:
+                        logging.info(f"Ignoring {namespace} : {from_address} -> {contract_address} ({name})")
+                        continue
+                    
             update_address(project_slug, contract_address, name, [chain], ['contract'])
-            logging.info(f"Added {contract_address} ({name}) in {project_slug}.yaml")
+
+        logging.info(f"No YAML for {namespace} : {from_address} -> {contract_address} ({name})")            
         
 
 if __name__ == "__main__":
