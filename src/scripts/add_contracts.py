67de954873d3122f632c6e9c3c7b7a9fcf7f8b86
Carl Cervone <carl.cervone@gmail.com>
2024-01-26 06:40:47 -0500

    feat: bulk add unique contracts

--- src/scripts/add_contracts.py
+++ src/scripts/add_contracts.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 import sys
 
-from map_artifacts import get_yaml_data_from_path, load_yaml_data
+from map_artifacts import get_yaml_data_from_path, load_yaml_data, map_addresses_to_slugs
 from update_project import update_address
 
 
@@ -26,21 +26,33 @@ def add_contracts_from_dune_export(filepath: str, chain: str) -> None:
         logging.error(f"File does not exist: {filepath}")
         return
     
-    chain = chain.lower().strip()
-    dune_data = pd.read_csv(filepath, usecols=['name', 'address', 'from'])
-
     project_yaml_data = get_yaml_data_from_path(LOCAL_PATH)
     yaml_data = {data['slug']: data for data in project_yaml_data}
+    
+    chain = chain.lower().strip()
+    address_to_slug_map = map_addresses_to_slugs(project_yaml_data, chain)
+    
+    # load the Dune data
+    dune_data = pd.read_csv(filepath, usecols=['namespace', 'name', 'from', 'address'])
+    dune_data['from'] = dune_data['from'].str.lower()
+    dune_data['address'] = dune_data['address'].str.lower()
 
-    with open(filepath, 'r') as file:
-        file_content = file.read()
-        json_data = json.loads(file_content)
-        contracts = json_data.get("data", [])
-        for contract in contracts:
-            name = contract.get("name", "")
-            address = contract.get("address", "")
-            networks = contract.get("networks", [])
-            tags = contract.get("tags", [])
-            update_address(project_slug, address, name, networks, tags)
+    # filter out contracts for creating tokens and LPs
+    dups = dune_data.groupby(['namespace', 'name', 'from'])['address'].nunique()
+    dups = dups[dups > 1].index
 
+    for _, row in dune_data.iterrows():
+        namespace = row['namespace']
+        name = row['name']
+        from_address = row['from']
+        if (namespace, name, from_address) in dups:
+            continue
+        contract_address = row['address']
+        if from_address in address_to_slug_map:
+            project_slug = address_to_slug_map[from_address]
+            update_address(project_slug, contract_address, name, [chain], ['contract'])
+            logging.info(f"Added {contract_address} ({name}) in {project_slug}.yaml")
+        
 
+if __name__ == "__main__":
+    add_contracts_from_dune_export(sys.argv[1], sys.argv[2])
