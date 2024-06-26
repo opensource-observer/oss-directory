81b077445ec2d0f359a0729b1e0a2cb6ec233711
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-06-26 23:37:47 +0000

    Report the repository metadata in library

--- src/ossdirectory/fetch.py
+++ src/ossdirectory/fetch.py
@@ -3,13 +3,22 @@ import yaml
 import os
 from typing import Callable, List, Optional, Any
 from dataclasses import dataclass
+from datetime import datetime
 
 from .schema import validate_project, validate_collection, ValidationResponse
 from .clone import temp_clone_repo
 
 
 @dataclass
+class OSSDirectoryMeta:
+    sha: str
+    committed_datetime: datetime
+    authored_datetime: datetime
+
+
+@dataclass(kw_only=True)
 class OSSDirectory:
+    meta: Optional[OSSDirectoryMeta] = None
     collections: List[dict]
     projects: List[dict]
 
@@ -58,5 +67,17 @@ def fetch_data(
     if directory_path:
         return _fetch_data(directory_path)
     else:
-        with temp_clone_repo(repo_url) as directory_path:
-            return _fetch_data(directory_path)
+        with temp_clone_repo(repo_url) as temp_repo:
+            head_commit = temp_repo.repo.head.commit
+            committed_datetime = head_commit.committed_datetime
+            authored_datetime = head_commit.authored_datetime
+            sha = head_commit.hexsha
+
+            directory_path = temp_repo.temp_dir
+            ossd = _fetch_data(directory_path)
+            ossd.meta = OSSDirectoryMeta(
+                sha=sha,
+                committed_datetime=committed_datetime,
+                authored_datetime=authored_datetime,
+            )
+            return ossd
