b91bc8dc24fcaca873876af140a1a96feff2ce4e
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-06-26 16:23:24 +0000

    fixes python exports

--- src/ossdirectory/__init__.py
+++ src/ossdirectory/__init__.py
@@ -1,5 +1,1 @@
-# try:
-#     import ossdirectory.resources
-# except ModuleNotFoundError:
-#     # The following section of code is included for compatibility when developing
-#     # locally. It is completely unused when this libary is installed by other users
+from .fetch import fetch_data
