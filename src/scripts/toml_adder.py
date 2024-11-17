7fbe157944231490c2f5cfbb02724d23ec13cf04
Carl Cervone <carl.cervone@gmail.com>
2024-11-17 07:03:38 +0700

    feat: update toml_adder to auto generate for entire collection

--- src/scripts/toml_adder.py
+++ src/scripts/toml_adder.py
@@ -9,9 +9,9 @@ from add_project import parse_url, generate_yaml
 from add_collection import generate_collection_yaml
 
 LOCAL_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "data", "projects")
-CRYPTO_SNAPSHOT = "crypto_ecosystems_snapshot.yaml"
-OSSD_SNAPSHOT = LOCAL_PATH + "/ossd_repo_snapshot.yaml"
 LOGGING_PATH = "data/logs/toml_adder.log"
+CRYPTO_SNAPSHOT = os.path.join("data", "logs", "crypto_ecosystems_snapshot.yaml")  # Updated path
+OSSD_SNAPSHOT = os.path.join("data", "logs", "ossd_repo_snapshot.yaml")  # Updated path
 
 SCHEMA_VERSION = 7
 
@@ -20,11 +20,10 @@ def map_crypto_ecosystems(ecosystems_path, load_snapshot=False):
     Creates a mapping of ecosystem titles to TOML file paths.
     '''
 
-    yaml_path = ecosystems_path + "/crypto_ecosystems_map.yaml"
+    yaml_path = CRYPTO_SNAPSHOT  # Updated to use new path
     if load_snapshot:
         with open(yaml_path, 'r', encoding='utf-8') as f:
             return yaml.safe_load(f)
-
 
     ecosystem_map = {}
     for root, dirs, files in os.walk(ecosystems_path):
@@ -43,38 +42,36 @@ def map_crypto_ecosystems(ecosystems_path, load_snapshot=False):
 
 def initialize_session():
     '''
-    Initializes a new session by creating a new log file and prompting the user to enter the path to the ecosystems directory.
-    Rteturns a dictionary containing the ecosystem name, the mapping of ecosystem titles to TOML file paths, and the snapshot of the repos already in oss directory.
+    Initializes a new session by creating a log file and asking the user for the path to the ecosystems directory.
+    Returns a dictionary with the ecosystem name, ecosystem mapping, and the snapshot of repositories in the oss directory.
     '''
     # Generate log for the session
     logging.basicConfig(filename=LOGGING_PATH, filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
     # Prompt user to enter the path to the ecosystems directory
-    ecosystems_path = input("Enter the path to the ecosystems directory: ")
+    ecosystems_path = input("Enter the path to the ecosystems directory: ").strip()
     while not os.path.isdir(ecosystems_path):
-        print(f"The directory '{ecosystems_path}' does not exist. Please enter a valid directory.")
-        ecosystems_path = input("Enter the path to the ecosystems directory: ")
-    create_new_snapshot = input("Do you want to load from a previous snapshot of the crypto ecosystems database? (yes/no): ").strip().lower()
-    if create_new_snapshot == 'yes':
+        print(f"Directory '{ecosystems_path}' does not exist. Please enter a valid directory.")
+        ecosystems_path = input("Enter the path to the ecosystems directory: ").strip()
+    
+    create_new_snapshot = input("Load a previous snapshot of the crypto ecosystems database? (Y/N): ").strip().lower()
+    if create_new_snapshot == 'y':
         crypto_ecosystems_map = map_crypto_ecosystems(ecosystems_path, load_snapshot=True)        
     else:
         crypto_ecosystems_map = map_crypto_ecosystems(ecosystems_path)
         
-
-    # Prompt user to take a name of the ecosystem to index
-    ecosystem_name = input("Enter the name of the ecosystem you want to index: ")
+    # Prompt user for the name of the ecosystem to index
+    ecosystem_name = input("Enter the name of the ecosystem to index: ").strip()
     while ecosystem_name not in crypto_ecosystems_map:
-        # Find names that are similar to their input and ask the user to try again
         similar_names = [name for name in crypto_ecosystems_map.keys() if ecosystem_name.lower() in name.lower()]
-        print(f"The ecosystem '{ecosystem_name}' does not exist. Did you mean one of the following?")
+        print(f"Ecosystem '{ecosystem_name}' not found. Did you mean one of these?")
         for name in similar_names:
             print(f"- {name}")
-        print()
-        ecosystem_name = input("Enter the name of the ecosystem you want to index: ")
+        ecosystem_name = input("Enter the name of the ecosystem to index: ").strip()
     
-    # Prompt user to take a snapshot of the repos already in oss directory
-    snapshot_option = input("Do you want to load from a previous snapshot of the repos already in oss directory? (yes/no): ").strip().lower()
-    if snapshot_option != 'yes':
+    # Prompt user to take a snapshot of the repos in the oss directory
+    snapshot_option = input("Load a previous snapshot of the repositories in the oss directory? (Y/N): ").strip().lower()
+    if snapshot_option != 'y':
         generate_repo_snapshot(OSSD_SNAPSHOT)
     with open(OSSD_SNAPSHOT, 'r') as f:
         ossd_repo_snapshot = yaml.safe_load(f)
@@ -100,10 +97,9 @@ def load_toml_file(file_path):
         return None, str(e)
     
 
-def process_project_toml_file(toml_path, ossd_repo_snapshot, write_file=True):
+def process_project_toml_file(toml_path, ossd_repo_snapshot, write_file=True, auto_generate="no"):
     """
-    Process a TOML file containing project information. It reads the TOML file,
-    extracts the title and github_organizations, and returns a list of slugs.
+    Processes a TOML file containing project information, extracts details, and returns a list of slugs.
     """
 
     toml_data, error = load_toml_file(toml_path)
@@ -113,7 +109,7 @@ def process_project_toml_file(toml_path, ossd_repo_snapshot, write_file=True):
 
     title = toml_data['title']
     github_orgs = toml_data['github_organizations']
-    print(f"\n\nProcessing TOML file at {toml_path} for project {title}, including the following GitHub organizations: {github_orgs}.")
+    print(f"\n\nProcessing TOML file at {toml_path} for project '{title}' with GitHub organizations: {github_orgs}.")
     slugs = []
     for github_org in github_orgs:
         url = github_org.lower().strip().strip("/")
@@ -128,26 +124,29 @@ def process_project_toml_file(toml_path, ossd_repo_snapshot, write_file=True):
                 continue            
             logging.info(f"Slug for {url} does not exist. Generating slug: {slug}")
             if write_file:
-                add_project = input(f"Add new project {slug} for {url}? (Y/N): ").strip().lower()
-                if add_project == 'q':
-                    return slugs
-                if add_project == 'y':
+                if auto_generate == 'yes':
                     generate_yaml(url, slug, title)
                     ossd_repo_snapshot[url] = slug
-                    logging.info(f"Added slug for {url} to ossd_repo_snapshot: {slug}")
+                    logging.info(f"Automatically added slug for {url} to ossd_repo_snapshot: {slug}")
                 else:
-                    logging.info(f"Skipping {url}")
-                    continue
+                    add_project = input(f"Add new project '{slug}' for {url}? (Y/N): ").strip().lower()
+                    if add_project == 'q':
+                        return slugs
+                    if add_project == 'y':
+                        generate_yaml(url, slug, title)
+                        ossd_repo_snapshot[url] = slug
+                        logging.info(f"Added slug for {url} to ossd_repo_snapshot: {slug}")
+                    else:
+                        logging.info(f"Skipping {url}")
+                        continue
         slugs.append(slug)
     return slugs
 
 
 def convert_collection_toml_file_to_json(ecosystem_name, crypto_ecosystems_map, ossd_repo_snapshot):
     """
-    Process a TOML file containing collection information. It reads the TOML file,
-    extracts the title and sub_ecosystems, and creates/returns a list of project slugs.
-    For each slug, it distinguishes between existing and new projects in OSSD.
-    Finally, it prepares a JSON file with the slugs, the repo, and their status.
+    Processes a TOML file containing collection information and creates/returns a list of project slugs.
+    Prepares a JSON file with slugs, repository details, and their status.
     """
 
     toml_path = crypto_ecosystems_map[ecosystem_name]
@@ -155,7 +154,7 @@ def convert_collection_toml_file_to_json(ecosystem_name, crypto_ecosystems_map, 
     if toml_data is None:
         logging.error(f"Error loading TOML data at {toml_path}: {error}")
         return
-    logging.info(f"Processing TOML file at {toml_path} for ecosystem {ecosystem_name}...")
+    logging.info(f"Processing TOML file at {toml_path} for ecosystem '{ecosystem_name}'...")
 
     sub_ecosystem_titles = toml_data['sub_ecosystems']
     data = []
@@ -175,17 +174,16 @@ def convert_collection_toml_file_to_json(ecosystem_name, crypto_ecosystems_map, 
                     "status": "existing" if exists else "new"
                 })
         else:
-            logging.error(f"Sub-ecosystem {title} does not exist.")
+            logging.error(f"Sub-ecosystem '{title}' does not exist.")
 
     json_path = f"temp/toml_{ecosystem_name.lower()}.json"
     with open(json_path, 'w') as file:
         json.dump(data, file, indent=4)
         
 
-def process_collection_toml_file(ecosystem_name, crypto_ecosystems_map, ossd_repo_snapshot):
+def process_collection_toml_file(ecosystem_name, crypto_ecosystems_map, ossd_repo_snapshot, auto_generate="no"):
     """
-    Process a TOML file containing collection information. It reads the TOML file,
-    extracts the title and sub_ecosystems, and creates/returns a list of project slugs.
+    Processes a TOML file containing collection information and creates/returns a list of project slugs.
     """
 
     toml_path = crypto_ecosystems_map[ecosystem_name]
@@ -193,21 +191,24 @@ def process_collection_toml_file(ecosystem_name, crypto_ecosystems_map, ossd_rep
     if toml_data is None:
         logging.error(f"Error loading TOML data at {toml_path}: {error}")
         return
-    logging.info(f"Processing TOML file at {toml_path} for ecosystem {ecosystem_name}...")
+    logging.info(f"Processing TOML file at {toml_path} for ecosystem '{ecosystem_name}'...")
 
     sub_ecosystem_titles = toml_data['sub_ecosystems']
     slugs = []
     for title in sub_ecosystem_titles:
         if title in crypto_ecosystems_map:
             sub_ecosystem_toml_path = crypto_ecosystems_map[title]
-            sub_ecosystem_slugs = process_project_toml_file(sub_ecosystem_toml_path, ossd_repo_snapshot)
+            sub_ecosystem_slugs = process_project_toml_file(sub_ecosystem_toml_path, ossd_repo_snapshot, auto_generate=auto_generate)
             if not sub_ecosystem_slugs:
                 continue
-            add_slugs = input(f"Add slugs {sub_ecosystem_slugs} for {title} to {ecosystem_name} collection? (Y/N): ").strip().lower()
-            if add_slugs == 'y':
+            if auto_generate != 'yes':
+                add_slugs = input(f"Add slugs {sub_ecosystem_slugs} for '{title}' to '{ecosystem_name}' collection? (Y/N): ").strip().lower()
+                if add_slugs == 'y':
+                    slugs.extend(sub_ecosystem_slugs)
+            else:
                 slugs.extend(sub_ecosystem_slugs)
         else:
-            logging.error(f"Sub-ecosystem {title} does not exist.")
+            logging.error(f"Sub-ecosystem '{title}' does not exist.")
 
     for github_organizations in toml_data['github_organizations']:
         url = github_organizations.lower().strip().strip("/")
@@ -221,15 +222,23 @@ def process_collection_toml_file(ecosystem_name, crypto_ecosystems_map, ossd_rep
                 logging.error(f"Error parsing URL: {url}")
                 continue            
             logging.info(f"Slug for {url} does not exist. Generating slug: {slug}")
-            add_project = input(f"Add new project {slug} for {url}? (Y/N): ").strip().lower()
-            if add_project == 'y':
-                display_name = input("Enter a display name for the project: ").strip()
+            if auto_generate != 'yes':
+                add_project = input(f"Add new project '{slug}' for {url}? (Y/N): ").strip().lower()
+                if add_project == 'y':
+                    display_name = input("Enter a display name for the project: ").strip()
+                    generate_yaml(url, slug, display_name)
+                    ossd_repo_snapshot[url] = slug
+                    logging.info(f"Added slug for {url} to ossd_repo_snapshot: {slug}")
+                else:
+                    logging.info(f"Skipping {url}")
+                    continue
+            else:
+                # Auto-generate project without prompt
+                display_name = title
                 generate_yaml(url, slug, display_name)
                 ossd_repo_snapshot[url] = slug
-                logging.info(f"Added slug for {url} to ossd_repo_snapshot: {slug}")
-            else:
-                logging.info(f"Skipping {url}")
-                continue
+                logging.info(f"Automatically added slug for {url} to ossd_repo_snapshot: {slug}")
+
         slugs.append(slug)
 
     slugs = sorted(list(set(slugs)))
@@ -240,6 +249,9 @@ def main(version=SCHEMA_VERSION):
     '''
     Main function that orchestrates the processing of ecosystems, projects, and collections.
     '''
+
+    # Prompt for auto-generation option
+    auto_generate = input("Auto-generate the collection without reviewing each project? (Y/N): ").strip().lower()
 
     # Initialize a new session
     session = initialize_session()
@@ -252,11 +264,11 @@ def main(version=SCHEMA_VERSION):
         return
 
     # Process the ecosystem as a collection
-    project_slugs = process_collection_toml_file(**session)
+    project_slugs = process_collection_toml_file(auto_generate=auto_generate, **session)
     if project_slugs:
         for slug in project_slugs:
             print(f"- {slug}")
-        make_collection = input(f"Make collection for {ecosystem_name}? (Y/N): ").strip().lower()
+        make_collection = input(f"Create a collection for '{ecosystem_name}'? (Y/N): ").strip().lower()
         if make_collection == 'y':
             collection_slug = input("Enter a slug for the collection: ").strip().lower()
             collection_name = input("Enter a name for the collection: ").strip()
