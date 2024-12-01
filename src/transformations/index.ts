da0db6bc9f2e0632990030e16bca5836f393c1dd
Carl Cervone <carl.cervone@gmail.com>
2024-11-30 20:52:00 -0500

    feat: transform all collections to alphabetical order

--- src/transformations/index.ts
+++ src/transformations/index.ts
@@ -5,12 +5,15 @@ import countNetworks from "./countNetworks.js";
 import eoaToAnyEvm from "./eoaToAnyEvm.js";
 import importAgora from "./importAgora.js";
 import convertToLower from "./convertToLower.js";
+import sortCollection from "./sortCollection.js";
+
 // List of migrations to run
 const TRANSFORMATIONS: Transformation[] = [
   eoaToAnyEvm,
   countNetworks,
   importAgora,
   convertToLower,
+  sortCollection,
 ];
 // The highest version number among all migrations
 const transformationNames = TRANSFORMATIONS.map((t) => t.name);
